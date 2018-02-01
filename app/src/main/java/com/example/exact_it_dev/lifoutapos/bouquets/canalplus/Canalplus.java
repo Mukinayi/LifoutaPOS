d2c39acfd771920bfe433edaf52d4e5607b57e77
EXACT-IT-DEV <mukinayiseth@outlook.com>
2018-02-01 16:59:55 +0100

    Ajout necessaire

--- app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/canalplus/Canalplus.java
+++ app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/canalplus/Canalplus.java
@@ -1,19 +1,45 @@
 package com.example.exact_it_dev.lifoutapos.bouquets.canalplus;
 
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.exact_it_dev.lifoutapos.R;
 
 public class Canalplus extends AppCompatActivity {
-
+    Button btncanalcheck;
+    EditText etcardnum;
+    ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canalplus);
         setTitle("Renouvellement canalplus");
 
+        btncanalcheck = (Button)findViewById(R.id.btncanalcheck);
+        etcardnum = (EditText)findViewById(R.id.etcardnum);
 
+        pDialog = new ProgressDialog(Canalplus.this);
+        pDialog.setMessage("Chargement...");
+        pDialog.setIndeterminate(true);
+        pDialog.setCancelable(false);
+
+
+        btncanalcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etcardnum.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Veuillez renseigner le numéro de la carte",Toast.LENGTH_SHORT).show();
+                }else{
+                    pDialog.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/easytv/EasyTV.java
+++ app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/easytv/EasyTV.java
@@ -1,26 +1,48 @@
 package com.example.exact_it_dev.lifoutapos.bouquets.easytv;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
+
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.exact_it_dev.lifoutapos.R;
+import com.example.exact_it_dev.lifoutapos.bouquets.canalplus.Canalplus;
 
 public class EasyTV extends AppCompatActivity {
-        MediaPlayer mediaPlayer;
+    Button btneasycheck;
+    EditText etcardnumeasy;
+    ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_tv);
         setTitle("Renouvellement Easy Tv");
 
-        //play the sound
-        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        btneasycheck = (Button)findViewById(R.id.btneasycheck);
+        etcardnumeasy = (EditText)findViewById(R.id.etcardnumeasy);
 
-        am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.easytv);
-        mediaPlayer.start();
+        pDialog = new ProgressDialog(EasyTV.this);
+        pDialog.setMessage("Chargement...");
+        pDialog.setIndeterminate(true);
+        pDialog.setCancelable(false);
+
+
+        btneasycheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etcardnumeasy.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Veuillez renseigner le numéro de la carte",Toast.LENGTH_SHORT).show();
+                }else{
+                    pDialog.show();
+                }
+            }
+        });
+
+
     }
 }
