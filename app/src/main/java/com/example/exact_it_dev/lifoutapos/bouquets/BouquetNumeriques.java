287502ead0dacf47a864b8f30a1ca1c13fc92252
EXACT-IT-DEV <mukinayiseth@outlook.com>
2018-02-01 13:45:17 +0100

    Ajout necessaire

--- app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/BouquetNumeriques.java
+++ app/src/main/java/com/example/exact_it_dev/lifoutapos/bouquets/BouquetNumeriques.java
@@ -15,23 +15,15 @@ import com.example.exact_it_dev.lifoutapos.bouquets.canalplus.Canalplus;
 import com.example.exact_it_dev.lifoutapos.bouquets.easytv.EasyTV;
 
 public class BouquetNumeriques extends AppCompatActivity {
-    Button canal,easy,startimes,dstv;
-    MediaPlayer mediaPlayer;
+    Button canal,easy,startimes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouquet_numeriques);
         setTitle("Les bouquet numériques");
 
-        //play the sound
-        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
-        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bouquets);
-        mediaPlayer.start();
-
         //Bouton canal
-        canal = (Button)findViewById(R.id.canalBT);
+        canal = (Button)findViewById(R.id.btncanaplus);
         canal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,36 +34,25 @@ public class BouquetNumeriques extends AppCompatActivity {
         });
 
         //Bouton Easy
-        easy = (Button)findViewById(R.id.easyBT);
+        easy = (Button)findViewById(R.id.btneasy);
         easy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent EasyIntent = new Intent(getApplicationContext(),EasyTV.class);
-                Toast.makeText(getApplicationContext(),"Vous avez cliqué sur Canal Easy TV",Toast.LENGTH_SHORT).show();
                 startActivity(EasyIntent);
             }
         });
 
         //Bouton Startime
-        startimes = (Button)findViewById(R.id.starttimeBT);
+        startimes = (Button)findViewById(R.id.btnstartime);
         startimes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent StartIntent = new Intent(getApplicationContext(),Canalplus.class);
-                Toast.makeText(getApplicationContext(),"Vous avez cliqué sur Canal Startime",Toast.LENGTH_SHORT).show();
                 startActivity(StartIntent);
             }
         });
 
-        //Bouton dstv
-        dstv = (Button)findViewById(R.id.dstvBT);
-        dstv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent DstvIntent = new Intent(getApplicationContext(),Canalplus.class);
-                Toast.makeText(getApplicationContext(),"Vous avez cliqué sur DSTV",Toast.LENGTH_SHORT).show();
-                startActivity(DstvIntent);
-            }
-        });
+
     }
 }
